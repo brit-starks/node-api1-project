5cf17ef0d540872782dd61684daa84c1b0f85aae
brit-starks <brittneytechiethomas@gmail.com>
2019-10-30 19:53:12 -0700

    Add user GET request (Success)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 
 const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 
@@ -9,8 +10,15 @@ server.listen(4000, () => {
 });
 
 server.get('/api/users', (req, res) => {
-  // console.log('you asked');
-  res.send('Hello Moto...')
+  db.find().then(users => {
+    res.status(200).json(users);
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: err,
+      success: false
+    });
+  });
 })
 
 // console.log("Hey Brit")
