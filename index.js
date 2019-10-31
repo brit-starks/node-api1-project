e3c1900d29eec9e518067063ad5b3b3ada27f5a5
brit-starks <brittneytechiethomas@gmail.com>
2019-10-30 19:33:03 -0700

    Add GET request

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log('server listening on port 4000')
+});
+
+server.get('/api/users', (req, res) => {
+  // console.log('you asked');
+  res.send('Hello Moto...')
+})
+
+// console.log("Hey Brit")
