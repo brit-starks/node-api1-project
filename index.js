4dd8e6cfa16e3bc3f6e712afddd92d55ca2c5da6
brit-starks <brittneytechiethomas@gmail.com>
2019-11-01 20:43:43 -0700

    Add DELETE and PUT request

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ server.listen(4000, () => {
   console.log('server listening on port 4000')
 });
 
+server.use(express.json());
+
 server.get('/api/users', (req, res) => {
   db.find().then(users => {
     res.status(200).json(users);
@@ -22,6 +24,7 @@ server.get('/api/users', (req, res) => {
 
 server.post('/api/users', (req, res) => {
   const userInfo = req.body;
+  // console.log(userInfo);
 
   db.insert(userInfo)
     .then((user) => {
@@ -36,4 +39,42 @@ server.post('/api/users', (req, res) => {
     });
 });
 
+server.delete('/api/users/:id', (req, res) =>{
+  const {id} = req.params;
+
+  db.remove(id).then(deletedUser => {
+    if (deletedUser) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({message: `Unable to find id=${id}`})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ error: "The user could not be removed"})
+  })
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const {id} = req.params;
+  const userInfo = req.body;
+
+  db.update(id, userInfo)
+  .then(user => {
+    if (user) {
+      res.status(200).json({
+        success: true, user
+      })
+    } else {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist."
+      })
+    }
+  })
+
+  .catch(err => {
+    res.status(500).json({
+      error: "The user information could not be modified.", err
+    })
+  })
+})
 // console.log("Hey Brit")
